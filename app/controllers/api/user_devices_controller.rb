a870b45e483c1e4de99385e07d3bdbf3a0cbe06d
Wonyoung So <mail@wonyoung.so>
2016-08-08 19:51:26 -0400

    update

--- app/controllers/api/user_devices_controller.rb
+++ app/controllers/api/user_devices_controller.rb
@@ -19,9 +19,9 @@ class Api::UserDevicesController < ApplicationController
           success: true, 
           status: @user_device.state,
           sim_id: @user_device.device.sim_card_id,
-          start_time: (@user_device.user.init_at + 3.hour),
+          start_time: (@user_device.user.init_at),
           minute_in_ms: @user_device.user.init_time,
-          personal_time: (@user_device.user.personal_time + 3.hour),
+          personal_time: (@user_device.user.personal_time),
           device_id: @device.human_id,
           tz_offset: @user_device.event_isopt.tz_offset
         }
