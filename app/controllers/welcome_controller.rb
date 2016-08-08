b6660606ca4178ab4c02bc8bd67365457c8738ad
Wonyoung So <mail@wonyoung.so>
2016-08-08 19:31:28 -0400

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @users = User.where(is_initialized: true)
+    @users = User.joins(:event_isopts).where("users.is_initialized = ? AND event_isopts.is_published = ?", true, true)
     @event_isopts = EventIsopt.where(is_resettable: false, is_published: true)
   end
 end
